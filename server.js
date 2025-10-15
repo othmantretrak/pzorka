e41286fe36a72bb32a034a7491b1d871f8339b96
tretrak <3trak10@gmail.com>
2025-10-15 18:11:32 +0100

    working - test vercel

--- server.js
+++ server.js
@@ -483,7 +483,8 @@ app.get("/logout", (req, res) => {
 // START SERVER
 // ============================================
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log("Login credentials: username: admin, password: wdf#2025");
-});
+}); */
+module.exports = app;
